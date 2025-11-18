e53fa9311701402a30da045813dbdb38c8744300
alexnoy <alexander.noy123@gmail.com>
2025-11-17 22:57:59 -0500

    Create tab-switching logic
    ClearPage is used inside the event listener to remove all DOM elements, then using the exported function from our module it will load the new DOM elements.

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import './styles.css';
+import { loadHome } from "./home.js";
+
+const content = document.getElementById('content');
+const nav = document.querySelector('nav');
+const homeNavButton = nav.querySelector('button');
+const menuNavButton = nav.querySelector('button:nth-child(2)');
+const aboutNavButton = nav.querySelector('button:last-of-type');
+
+const clearPage = () => {
+    content.childNodes.forEach((childNode) => {
+        childNode.remove();
+    });
+}
+
+const createHomePage = () => {
+    clearPage();
+    loadHome();
+}
+homeNavButton.addEventListener('click', createHomePage);
