32082e4403dc2d76a3ca0c1890478c9d8794fa43
alexnoy <alexander.noy123@gmail.com>
2025-11-18 22:35:19 -0500

    Add drinks section to menu page
    The drinkSection is imported and appended to the menuSection along with titles to indicate which section is which.

--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,17 @@
 import { sushiSection } from './menu-items/sushi.js';
+import { drinkSection } from './menu-items/drinks.js';
 import { content } from './index.js';
 
 const menuContent = document.createElement('div');
 menuContent.classList.add('menu-page');
 
-menuContent.append(sushiSection);
+const sushiMenuTitle = document.createElement('h1');
+sushiMenuTitle.textContent = 'Sushi';
+
+const drinkMenuTitle = document.createElement('h1');
+drinkMenuTitle.textContent = 'Drinks';
+
+menuContent.append(sushiMenuTitle, sushiSection, drinkMenuTitle, drinkSection);
 
 export const loadMenu = () => {
     content.appendChild(menuContent);
