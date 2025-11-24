b5ebbff563d5ee2c1a4ece208e3d40c39d5707d8
alexnoy <alexander.noy123@gmail.com>
2025-11-23 21:17:39 -0500

    Add extra divs for each menu section
    The divs allow for more flexibility when styling.

--- src/menu.js
+++ src/menu.js
@@ -1,17 +1,27 @@
-import { sushiSection } from './menu-items/sushi.js';
-import { drinkSection } from './menu-items/drinks.js';
+import { sushiCardsDiv } from './menu-items/sushi.js';
+import { drinkCardsDiv } from './menu-items/drinks.js';
 import { content } from './index.js';
 
 const menuContent = document.createElement('div');
 menuContent.classList.add('menu-page');
 
+const sushiSection = document.createElement('div');
+sushiSection.classList.add('sushi-section');
+
 const sushiMenuTitle = document.createElement('h1');
 sushiMenuTitle.textContent = 'Sushi';
+
+sushiSection.append(sushiMenuTitle, sushiCardsDiv);
+
+const drinkSection = document.createElement('div');
+drinkSection.classList.add('drink-section');
 
 const drinkMenuTitle = document.createElement('h1');
 drinkMenuTitle.textContent = 'Drinks';
 
-menuContent.append(sushiMenuTitle, sushiSection, drinkMenuTitle, drinkSection);
+drinkSection.append(drinkMenuTitle, drinkCardsDiv);
+
+menuContent.append(sushiSection, drinkSection);
 
 export const loadMenu = () => {
     content.appendChild(menuContent);
